72d2fb2d5763e1ff6995c098fa9ed4c9df75d163
stark <wlsdks12@naver.com>
2026-07-15 10:38:37 +0900

    refactor: re-export shared key serializer in messaging queue helper

--- packages/messaging/src/file-mutation-queue.ts
+++ packages/messaging/src/file-mutation-queue.ts
@@ -1,5 +1,1 @@
-import { serializePerKey } from "@muse/shared";
-
-export function serializePerFile<T>(queues: Map<string, Promise<unknown>>, file: string, op: () => Promise<T>): Promise<T> {
-  return serializePerKey(queues, file, op);
-}
+export { serializePerKey as serializePerFile } from "@muse/shared";
